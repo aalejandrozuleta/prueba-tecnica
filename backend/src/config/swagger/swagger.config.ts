7b2f65e3e076e9ddb66b31d9280ba97749837169
aalejandrozuleta <jhoitan266@gmail.com>
2026-01-10 14:58:17 -0500

    swagger docs

--- backend/src/config/swagger/swagger.config.ts
+++ backend/src/config/swagger/swagger.config.ts
@@ -4,15 +4,11 @@ import { DocumentBuilder } from '@nestjs/swagger';
  * Configuración base de Swagger / OpenAPI.
  */
 export const swaggerConfig = new DocumentBuilder()
-  .setTitle('prueba técnica api')
+  .setTitle('Prueba técnica API')
   .setDescription('Documentación oficial de la API')
   .setVersion('1.0.0')
-  .addBearerAuth(
-    {
-      type: 'http',
-      scheme: 'bearer',
-      bearerFormat: 'JWT',
-    },
-    'access-token',
-  )
+  .addCookieAuth('access_token', {
+    type: 'apiKey',
+    in: 'cookie',
+  })
   .build();
